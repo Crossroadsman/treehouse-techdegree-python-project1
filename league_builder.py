9fbd0da4e13b1d30fa07f676d0b5bf98ab037076
CrossroadsMan <crossroadsman@gmail.com>
2018-03-29 15:19:30 -0600

    refactor csv reading into function

--- league_builder.py
+++ league_builder.py
@@ -1,6 +1,27 @@
 import csv
 
+filename = 'soccer_players.csv'
+
+# Read the CSV data
+# -----------------
+def csv_to_dictlist(filename):
+    output_dictlist = []
+
+    with open(filename, newline='') as csv_file:
+        dict_reader = csv.DictReader(csv_file)
+        rows = list(dict_reader)
+        for row in rows:
+            output_dictlist.append(row)
+
+    return output_dictlist
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+
+    players = csv_to_dictlist(filename=filename)
+
+    for player in players:
+        for key in player.keys():
+            print("{}: {}".format(key, player[key]))
+
 
