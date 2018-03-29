dd83072381a9f9a0c131078a342873ac41f4199a
CrossroadsMan <crossroadsman@gmail.com>
2018-03-29 16:57:25 -0600

    Add extra credit

--- league_builder.py
+++ league_builder.py
@@ -77,30 +77,75 @@ def create_teams_list(team_names, team_rosters):
 
 
     if len(team_rosters[len(team_names)]) != 0:
-        print("INVALID PLAYERS")
-        print("===============")
+        output_text.append("Unallocated Players")
         for player in team_rosters[len(team_names)]:
-            print(player['Name'])
+            player_details = ", ".join(player.values())
+            output_text.append(player_details)
 
     return output_text
 
 
+# Write teams list to file
+# ------------------------
+def create_file(filename, datalist):
+    with open(filename, 'w') as file_handle:
+        for row in datalist:
+            file_handle.write(row)
+            file_handle.write("\n")
+
+# Create filename from name
+# -------------------------
+def create_filename_from_player(player):
+    name_components = player['Name'].split()
+    joined_components = "_".join(name_components)
+    return "{}.txt".format(joined_components)
+
+# Create welcome letter
+# ---------------------
+def create_welcome_letter(team_name, player, first_practice_datetime):
+    output_text = []
+    output_text.append('Dear {}:'.format(player['Guardian Name(s)']))
+    output_text.append('We are delighted to welcome {} to the {}.'.format(player['Name'], team_name))
+    output_text.append("{}'s first practice will be on {}.".format(player['Name'], first_practice_datetime))
+    output_text.append("Yours truly, League Coordinator")
+    return output_text
+
+# Create letters
+# --------------
+def create_letters(team_names, team_rosters, first_practice_datetime):
+    for i in range(len(team_names)):
+        for player in team_rosters[i]:
+            letter = create_welcome_letter(team_name=team_names[i],
+                                           player=player,
+                                           first_practice_datetime=first_practice_datetime)
+            filename = create_filename_from_player(player)
+            create_file(filename=filename, datalist=letter)
+
+        
+
+
 if __name__ == "__main__":
     
-    filename = 'soccer_players.csv'
-    # filename = 'invalid_players.csv'
+    input_filename = 'soccer_players.csv'
+    # input_filename = 'invalid_players.csv'
+    output_filename = 'teams.txt'
 
     teams = ['Sharks',
              'Dragons',
              'Raptors',
             ]
 
-    players = csv_to_dictlist(filename=filename)
+    first_practice_datetime = "April 15, 2018"
+
+    players = csv_to_dictlist(filename=input_filename)
     
     team_rosters = allocate_players(players=players, teams=teams)
 
     team_details = create_teams_list(team_rosters=team_rosters,
                                      team_names=teams)
 
-    for row in team_details:
-        print(row)
+    create_file(filename=output_filename, datalist=team_details)
+
+    create_letters(team_names=teams,
+                   team_rosters=team_rosters,
+                   first_practice_datetime=first_practice_datetime)
